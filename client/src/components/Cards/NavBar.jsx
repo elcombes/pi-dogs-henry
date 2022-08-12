1f4c80b6a89d300fed9da436af5060a7126c35c5
Emmanuel Combes <emmanuel.l.combes@gmail.com>
2022-08-12 17:11:29 -0300

    NavBar css

--- client/src/components/Cards/NavBar.jsx
+++ client/src/components/Cards/NavBar.jsx
@@ -4,7 +4,18 @@ import styles from '../../css/NavBar.module.css';
 export default function NavBar() {
     return (
         <div>
-            <div className={styles.navbar}><h1>Henry Dogs</h1></div>
+            <div className={styles.navbar}>
+                <div><img className={styles.logoimg} src="logoapp.png" alt="logoapp" /></div>
+                <div><span className={styles.appTittle}>Henry Dogs</span></div>
+                <div>
+                    <ul className={styles.menulist}>
+                        <li>Home</li>
+                        <li>Create +</li>
+                        <li>About me</li>
+                    </ul>
+                </div>
+            </div>
+
         </div>
     )
 }
